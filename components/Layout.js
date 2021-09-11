8499933e6318a6b4ee54a31a67296236e707a0d2
abel <abelorode@gmail.com>
2021-09-11 09:17:09 +0100

    WD-1472 feat: integrated next router

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/Layout.module.css"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import {useRouter} from "next/router"
+import styles from "@/styles/Layout.module.css"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Showcase from "@/components/Showcase";
 
 export default function Layout({title, description, keywords, children}){
+    const router = useRouter();
   return(
       <div>
           <Head>
@@ -13,6 +16,7 @@ export default function Layout({title, description, keywords, children}){
           </Head>
 
           <Header/>
+          {router.pathname === '/' && <Showcase/>}
           <div className={styles.container}>
             {children} 
           </div>
